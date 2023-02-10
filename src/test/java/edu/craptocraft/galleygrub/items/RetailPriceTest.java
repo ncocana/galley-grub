44bcff1b10e16f69c135a8aaa9a902ce70512c37
ncocana <ncocana@cifpfbmoll.eu>
2023-02-10 19:58:34 +0100

    refactor(RetailPriceTest): create a setup

--- src/test/java/edu/craptocraft/galleygrub/items/RetailPriceTest.java
+++ src/test/java/edu/craptocraft/galleygrub/items/RetailPriceTest.java
@@ -1,16 +1,22 @@
 package edu.craptocraft.galleygrub.items;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RetailPriceTest {
     
-    @Test
-    public void testGetPriceExtra() {
+    @Before
+    public void setup_initPrices() {
 
         RetailPrice.init_prices();
         
+    }
+
+    @Test
+    public void testGetPriceExtra() {
+
         assertEquals(Double.valueOf(0.25), RetailPrice.getPrice("cheese"));
         assertEquals(Double.valueOf(0.25), RetailPrice.getPrice("medium"));
         assertEquals(Double.valueOf(0.5), RetailPrice.getPrice("large"));
@@ -21,8 +27,6 @@ public class RetailPriceTest {
     @Test
     public void testContainsKeyExtra() {
 
-        RetailPrice.init_prices();
-        
         assertTrue("This extra doesn't exist in the Map", RetailPrice.contains("cheese"));
         assertTrue("This extra doesn't exist in the Map", RetailPrice.contains("sauce"));
         assertTrue("This extra doesn't exist in the Map", RetailPrice.contains("medium"));
