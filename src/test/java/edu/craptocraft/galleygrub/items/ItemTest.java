ffd00e81abf6a7d067caf1b3522bfb0c1f1158b6
ncocana <ncocana@cifpfbmoll.eu>
2023-02-10 19:01:51 +0100

    test(ItemTest): add test for Item class

---
+++ src/test/java/edu/craptocraft/galleygrub/items/ItemTest.java
@@ -0,0 +1,19 @@
+package edu.craptocraft.galleygrub.items;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+    
+    @Test
+    public void testGetPriceExtra() {
+
+        Product patty = new Item("Krabby Patty", 1.25);
+        Product pattyRepeat = new Item("Krabby Patty", 1.00);
+        
+        assertTrue(patty.equals(pattyRepeat));
+
+    }
+
+}
