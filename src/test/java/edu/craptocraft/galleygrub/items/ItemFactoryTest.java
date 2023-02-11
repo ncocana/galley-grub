ba0eabc53c3b7a6eb464482eac2af962215f7c4a
ncocana <ncocana@cifpfbmoll.eu>
2023-02-11 22:19:48 +0100

    refactor(ItemFactoryTest): reduce imports

--- src/test/java/edu/craptocraft/galleygrub/items/ItemFactoryTest.java
+++ src/test/java/edu/craptocraft/galleygrub/items/ItemFactoryTest.java
@@ -3,7 +3,10 @@ package edu.craptocraft.galleygrub.items;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
 public class ItemFactoryTest {
     
