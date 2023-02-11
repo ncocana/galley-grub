d50e68836297af0be61d8016838773a8bf72ccdf
ncocana <ncocana@cifpfbmoll.eu>
2023-02-11 21:02:00 +0100

    refactor(ItemTest, RetailPriceTest): update imports

--- src/test/java/edu/craptocraft/galleygrub/items/RetailPriceTest.java
+++ src/test/java/edu/craptocraft/galleygrub/items/RetailPriceTest.java
@@ -3,7 +3,8 @@ package edu.craptocraft.galleygrub.items;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RetailPriceTest {
     
